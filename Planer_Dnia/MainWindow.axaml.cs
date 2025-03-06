8b9c29ee5a3bb1b0a02088a42e6f6890cb847cb0
igor <igor.kowalczyk07@poczta.fm>
2025-03-06 22:00:11 +0100

    added app interface for adding tasks

--- Planer_Dnia/MainWindow.axaml.cs
+++ Planer_Dnia/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace Planer_Dnia;
 
@@ -7,5 +8,10 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void submitOne(object? sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
     }
 }

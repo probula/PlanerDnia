62bb8c8a4136f76cfdbaa4dbfb7758c421576ad0
igor <igor.kowalczyk07@poczta.fm>
2025-03-07 23:25:03 +0100

    created ability to add task

--- Planer_Dnia/MainWindow.axaml.cs
+++ Planer_Dnia/MainWindow.axaml.cs
@@ -12,10 +12,17 @@ public partial class MainWindow : Window
 
     private void submitOne(object? sender, RoutedEventArgs e)
     {
-        throw new System.NotImplementedException();
+        tbOne.Text = textBoxOne.Text;
+        var comboBoxValue = (comboBoxOne.SelectedItem as ComboBoxItem)?.Content?.ToString() ?? "No selection";
+        showTB.Text=comboBoxValue;
     }
 
     private void submitTwo(object? sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void edit(object? sender, RoutedEventArgs e)
     {
         throw new System.NotImplementedException();
     }
